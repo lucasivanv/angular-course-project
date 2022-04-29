8cce8ad708daf4ab2e0dd0d6681db554639ea385
Lucas Ivan <30575834+lucasivanv@users.noreply.github.com>
2022-04-28 22:16:26 -0300

    Adding ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,29 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe-model";
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A test recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
-        new Recipe('Another test recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')
+        new Recipe(
+            'Vegan burger',
+            'A plant-based burger',
+            'https://elavegan.com/wp-content/uploads/2019/09/Plant-based-burger-with-oil-free-french-fries.jpg',
+            [
+                new Ingredient('Plant-based burger', 1),
+                new Ingredient('French Fries', 20),
+            ]
+        ),
+        new Recipe(
+            'Vegan lasagna',
+            'An amazing meatless lasagna',
+            'https://thehiddenveggies.com/wp-content/uploads/2020/08/vegan-lasagna-sq.jpg',
+            [
+                new Ingredient('Lasagna noodles', 2),
+                new Ingredient('Mushrooms', 1),
+            ]
+        )
     ];
 
     getRecipes() {
