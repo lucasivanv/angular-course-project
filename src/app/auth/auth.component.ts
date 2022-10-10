7db81c45bd9cf0746959a4c1df00c0225599bc34
Lucas Ivan <30575834+lucasivanv@users.noreply.github.com>
2022-10-09 21:02:35 -0300

    Sending the signup request

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,19 +1,32 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html',
 })
 export class AuthComponent {
-
     isLoginMode = true;
+
+    constructor(
+        private authService: AuthService
+    ) { }
 
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
     }
 
     onSubmit(form: NgForm) {
+        if (!form.valid) return;
+
+        const { email, password } = form.value;
+
+        if (this.isLoginMode) {
+            // TODO
+        } else {
+            this.authService.signup(email, password).subscribe();
+        }
         form.reset();
     }
 }
