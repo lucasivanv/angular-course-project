58d71bddd7a86c7cd24a2f05f63d9d360ea52e18
Lucas Ivan <30575834+lucasivanv@users.noreply.github.com>
2022-10-11 18:54:34 -0300

    Creating and storing the user data

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, throwError } from 'rxjs';
+import { catchError, Subject, tap, throwError } from 'rxjs';
+import { User } from './user.model';
 
 export interface AuthResponseData {
     idToken: string;
@@ -13,6 +14,7 @@ export interface AuthResponseData {
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+    user = new Subject<User>();
 
     constructor(
         private http: HttpClient
@@ -25,7 +27,12 @@ export class AuthService {
                 password, 
                 returnSecureToken: true
             }
-        ).pipe(catchError(this.handleError))
+        ).pipe(
+            catchError(this.handleError),
+            tap(responseData => {
+                this.handleAuthentication(responseData.email, responseData.localId, responseData.idToken, +responseData.expiresIn);
+            })
+        )
     }
 
     login(email: string, password: string) {
@@ -35,7 +42,18 @@ export class AuthService {
                 password, 
                 returnSecureToken: true
             }
-        ).pipe(catchError(this.handleError))
+        ).pipe(
+            catchError(this.handleError),
+            tap(responseData => {
+                this.handleAuthentication(responseData.email, responseData.localId, responseData.idToken, +responseData.expiresIn);
+            })
+        )
+    }
+
+    private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+        const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+        const user = new User(email, userId, token, expirationDate);
+        this.user.next(user);
     }
 
     private handleError(errorResponse: HttpErrorResponse) {
