7c2304deb81e7efefe9863bd0a8e669babfd2465
Lucas Ivan <30575834+lucasivanv@users.noreply.github.com>
2022-10-24 20:57:05 -0300

    Understanding core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,35 @@
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AuthComponent } from './auth/auth.component';
+import { CoreModule } from './core.module';
 import { HeaderComponent } from './header/header.component';
-import { RecipeService } from './recipes/recipe.service';
 import { RecipesModules } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared/shared.module';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { ShoppingListModule } from './shopping-list/shopping.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    AuthComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    RecipesModules,
-    ShoppingListModule,
-    SharedModule
-  ],
-  providers: [ShoppingListService, RecipeService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        AuthComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        RecipesModules,
+        ShoppingListModule,
+        SharedModule,
+        CoreModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
